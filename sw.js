225158a9984e90a9f40d67e35f0a56d0cb1afb41
catdogmaus <116189627+catdogmaus@users.noreply.github.com>
2025-11-12 21:55:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,9 @@ const assets = [
     "/D30printerPWA/",
     "/D30printerPWA/index.html",
     "/D30printerPWA/js/app.js",
-    "/D30printerPWA/css/style.css",
-    "/D30printerPWA/dist/esc-pos-encoder.js",
-    "/D30printerPWA/images/icons/icon-72x72.png",
-    "/D30printerPWA/images/icons/icon-96x96.png",
-    "/D30printerPWA/images/icons/icon-128x128.png",
-    "/D30printerPWA/images/icons/icon-144x144.png",
-    "/D30printerPWA/images/icons/icon-152x152.png",
-    "/D30printerPWA/images/icons/icon-192x192.png",
-    "/D30printerPWA/images/icons/icon-384x384.png",
-    "/D30printerPWA/images/icons/icon-512x512.png"
+    "/D30printerPWA/dist/esc-pos-encoder.js"
 ];
 
-// install event
 self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open("static").then((cache) => {
@@ -23,7 +13,6 @@ self.addEventListener("install", (e) => {
     );
 });
 
-// PWA CRITICAL: A fetch event handler is required for the browser to prompt installation
 self.addEventListener("fetch", (e) => {
     e.respondWith(
         caches.match(e.request).then((response) => {
