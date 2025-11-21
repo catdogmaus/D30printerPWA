29dbbf9b9f322a2ad6db51efc36b8b8427eba002
catdogmaus <116189627+catdogmaus@users.noreply.github.com>
2025-11-21 18:57:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'd30-pwa-v7'; // Incremented version to force update
+const CACHE_NAME = 'd30-pwa-v8'; // Incremented version to force update
 const ASSETS = [
   './',
   './index.html',
